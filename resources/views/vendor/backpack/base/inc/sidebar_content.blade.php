5aa85ed5f283f7cb10c31eb1079ae5c0308a0e2d
King-Mike <nsubugamike021@gmail.com>
2020-03-05 17:13:08 +0300

    Changed side bar icons

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,6 +1,15 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class=nav-item><a class=nav-link href="{{ backpack_url('elfinder') }}"><i class="nav-icon fa fa-files-o"></i> <span>{{ trans('backpack::crud.file_manager') }}</span></a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('tag') }}'><i class='nav-icon fa fa-question'></i> Tags</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('type') }}'><i class='nav-icon fa fa-question'></i> Types</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('resource') }}'><i class='nav-icon fa fa-question'></i> Resources</a></li>
+<li class=nav-item><a class=nav-link href="{{ backpack_url('elfinder') }}"><i class="nav-icon fa fa-file"></i> <span>{{ trans('backpack::crud.file_manager') }}</span></a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('tag') }}'><i class='nav-icon fa fa-tags'></i> Tags</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('type') }}'><i class='nav-icon fa fa-navicon'></i> Types</a></li>
+<li class='nav-item'><a class='nav-link' href='{{ backpack_url('resource') }}'><i class='nav-icon fa fa-clone'></i> Resources</a></li>
+<!-- Users, Roles, Permissions -->
+<li class="nav-item nav-dropdown">
+	<a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon fa fa-group"></i> Authentication</a>
+	<ul class="nav-dropdown-items">
+	  <li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon fa fa-user"></i> <span>Users</span></a></li>
+	  <li class="nav-item"><a class="nav-link" href="{{ backpack_url('role') }}"><i class="nav-icon fa fa-group"></i> <span>Roles</span></a></li>
+	  <li class="nav-item"><a class="nav-link" href="{{ backpack_url('permission') }}"><i class="nav-icon fa fa-key"></i> <span>Permissions</span></a></li>
+	</ul>
+</li>
