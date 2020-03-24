6ea4f04da2d1187cf26b91f3dba9a18877fbec02
King-Mike <nsubugamike021@gmail.com>
2020-03-24 07:51:42 +0300

    created Resource observer

---
+++ app/Observers/ResourceObserver.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Observers;
+
+use App\Resource;
+
+class ResourceObserver
+{
+    /**
+     * Handle the resource "created" event.
+     *
+     * @param  \App\Resource  $resource
+     * @return void
+     */
+    public function created(Resource $resource)
+    {
+        //
+    }
+
+    /**
+     * Handle the resource "updated" event.
+     *
+     * @param  \App\Resource  $resource
+     * @return void
+     */
+    public function updated(Resource $resource)
+    {
+        //
+    }
+
+    /**
+     * Handle the resource "deleted" event.
+     *
+     * @param  \App\Resource  $resource
+     * @return void
+     */
+    public function deleted(Resource $resource)
+    {
+        //
+    }
+
+    /**
+     * Handle the resource "restored" event.
+     *
+     * @param  \App\Resource  $resource
+     * @return void
+     */
+    public function restored(Resource $resource)
+    {
+        //
+    }
+
+    /**
+     * Handle the resource "force deleted" event.
+     *
+     * @param  \App\Resource  $resource
+     * @return void
+     */
+    public function forceDeleted(Resource $resource)
+    {
+        //
+    }
+}
